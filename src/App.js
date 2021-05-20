29567c9d54c0d206c872ae871d4b44ba0b177ab4
Said Osman <said_henry@hotmail.com>
2021-05-20 18:47:25 +0200

    Part One

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './utils/global/css/Global.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>h1</h1>
+      <h2>h2</h2>
+      <h3>h3</h3>
+      <p>p</p>
+      <p className='secondary_text'>p</p>
+      <p className='control_labels'>p</p>
+      <button className='primary_button'>button</button>
+    </>
   );
 }
 
